e1781932a50d51c05a9ef4697e93b6babdd86580
Danna Paola <dannacapera16@gmail.com>
2023-11-10 23:36:10 -0500

    Push of entire project frontend application progress, missing small details to fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HomePage } from './components/HomePage';
+import { Login } from './components/Login';
+import { CustomerMenu } from './components/CustomerMenu';
+import { EmployeeMenu } from './components/EmployeeMenu';
+import { AdministratorMenu } from './components/AdministratorMenu';
+import { AdministratorProfile } from './components/AdministratorProfile';
+import { EmployeeProfile } from './components/EmployeeProfile';
+import { CreateTables } from './components/CreateTables';
+import { ConsultTables } from './components/ConsultTables';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/Login" element={<Login />} />
+          <Route exact path="/EmployeeMenu/:user" element={<EmployeeMenu />} />
+          <Route exact path="/CustomerMenu/:user" element={<CustomerMenu />} />
+          <Route
+            exact
+            path="/AdministratorMenu/:user"
+            element={<AdministratorMenu />}
+          />
+          <Route
+            exact
+            path="/AdministratorMenu/:user/Profile"
+            element={<AdministratorProfile />}
+          />
+          <Route
+            exact
+            path="/EmployeeMenu/:user/Profile"
+            element={<EmployeeProfile />}
+          />
+          <Route
+            exact
+            path="/AdministratorMenu/:user/CreateTables"
+            element={<CreateTables />}
+          />
+          <Route
+            exact
+            path="/AdministratorMenu/:user/ConsultTables"
+            element={<ConsultTables />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
